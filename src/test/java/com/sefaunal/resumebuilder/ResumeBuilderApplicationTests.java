50083d5d39dcc267a003968098eba7b8c9a31c29
sefaunal <83312431+sefaunal@users.noreply.github.com>
2024-01-13 20:49:04 +0300

    Initial Commit

---
+++ src/test/java/com/sefaunal/resumebuilder/ResumeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sefaunal.resumebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
